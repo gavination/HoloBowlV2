053196acc54bfbbfc1eb92dd7bca51d37b98d18e
Gavin Bauman <gabauman@microsoft.com>
2017-07-18 13:51:36 -0400

    Initial Commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class Ball : MonoBehaviour, IFocusable, IInputClickHandler
+{
+
+    public Vector3 TapForce = new Vector3(0f, 3f, 600f);
+    new Rigidbody rigidbody;
+
+    private bool gazing = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+       
+
+        if (rigidbody == null)
+        {
+            rigidbody = gameObject.AddComponent<Rigidbody>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gazing == false) return;
+
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        rigidbody.AddForce(TapForce);
+     
+
+    }
+
+    public void OnFocusEnter()
+    {
+        gazing = true;    }
+
+    public void OnFocusExit()
+    {
+        gazing = false;
+    }
+}
---
+++ Assets/Scripts/BallCommands.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCommands : MonoBehaviour {
+
+    public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CreateBall()
+    {
+        Vector3 ballPosition = Camera.main.transform.position + Camera.main.transform.forward * 1f;
+        //Vector3 BallDirection = Camera.main.transform.forward;
+
+        Instantiate(ball, ballPosition, Quaternion.identity);
+
+
+    }
+}
